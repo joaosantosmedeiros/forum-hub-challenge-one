861332625d75171958b1b420f92745fb8262e7dd
joaosantosmedeiros <jopesame@gmail.com>
2024-07-01 09:24:53 -0300

    feat: add topic entity

---
+++ src/main/java/com/jota/hub/challenge/domain/topic/Topic.java
@@ -0,0 +1,36 @@
+package com.jota.hub.challenge.domain.topic;
+
+import com.jota.hub.challenge.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "TOPICS")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Topic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String message;
+
+    private LocalDateTime creationTime;
+
+    @Enumerated(value = EnumType.STRING)
+    private TopicStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "authorId")
+    private User author;
+}
