8f74d22768893a8574c2eb8d39a8b255808bbbad
joaosantosmedeiros <jopesame@gmail.com>
2024-07-01 10:35:32 -0300

    refactor: rename create topic dto

--- src/main/java/com/jota/hub/challenge/domain/topic/CreateTopicDTO.java
+++ src/main/java/com/jota/hub/challenge/domain/topic/CreateTopicDTO.java
@@ -2,7 +2,7 @@ package com.jota.hub.challenge.domain.topic;
 
 import com.jota.hub.challenge.domain.user.User;
 
-public record TopicDTO(
+public record CreateTopicDTO(
         String message,
         String title,
         User author
