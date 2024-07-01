d1ceba64f801bb7f8c9d88d6a399be0c08b7c45d
joaosantosmedeiros <jopesame@gmail.com>
2024-07-01 10:41:37 -0300

    feat: add unique constraint in user entity email field

--- src/main/java/com/jota/hub/challenge/domain/user/User.java
+++ src/main/java/com/jota/hub/challenge/domain/user/User.java
@@ -18,10 +18,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(unique = true, nullable = false)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 
     @OneToMany(mappedBy = "author")
