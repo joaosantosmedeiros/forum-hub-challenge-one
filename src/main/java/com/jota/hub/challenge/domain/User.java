48df32097ec36c9af737d0f9b7201a0780624d75
joaosantosmedeiros <jopesame@gmail.com>
2024-07-01 09:00:48 -0300

    feat: add user entity

---
+++ src/main/java/com/jota/hub/challenge/domain/User.java
@@ -0,0 +1,20 @@
+package com.jota.hub.challenge.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "USERS")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+}
