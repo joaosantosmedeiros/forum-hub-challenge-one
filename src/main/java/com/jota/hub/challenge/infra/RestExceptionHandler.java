7fa7280ebdad21a757bfee8b7c858d1cd9bcc099
joaosantosmedeiros <jopesame@gmail.com>
2024-07-01 10:54:46 -0300

    feat: add illegal argument exception in exception handler

--- src/main/java/com/jota/hub/challenge/infra/RestExceptionHandler.java
+++ src/main/java/com/jota/hub/challenge/infra/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.jota.hub.challenge.infra;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,5 +13,10 @@ public class RestExceptionHandler {
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity notFound() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity badRequest(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 }
