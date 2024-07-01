6e082169064b86efba60cb6557b81846e6732dd1
joaosantosmedeiros <jopesame@gmail.com>
2024-07-01 09:25:17 -0300

    feat: add bidiretional relationship in user and topic entities

--- src/main/java/com/jota/hub/challenge/domain/user/User.java
+++ src/main/java/com/jota/hub/challenge/domain/user/User.java
@@ -1,7 +1,10 @@
-package com.jota.hub.challenge.domain;
+package com.jota.hub.challenge.domain.user;
 
+import com.jota.hub.challenge.domain.topic.Topic;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.List;
 
 @Entity
 @Table(name = "USERS")
@@ -14,7 +17,13 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String email;
+
     private String password;
+
+    @OneToMany(mappedBy = "author")
+    private List<Topic> topics;
 }
