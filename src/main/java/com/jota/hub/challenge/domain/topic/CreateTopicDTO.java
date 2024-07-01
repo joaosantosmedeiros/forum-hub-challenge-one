54e8568b7547f977a7afcccfd20e36824ee6e9bd
joaosantosmedeiros <jopesame@gmail.com>
2024-07-01 10:40:21 -0300

    feat: add validations in create topic dto

--- src/main/java/com/jota/hub/challenge/domain/topic/CreateTopicDTO.java
+++ src/main/java/com/jota/hub/challenge/domain/topic/CreateTopicDTO.java
@@ -1,9 +1,12 @@
 package com.jota.hub.challenge.domain.topic;
 
 import com.jota.hub.challenge.domain.user.User;
+import jakarta.validation.constraints.NotBlank;
 
 public record CreateTopicDTO(
+        @NotBlank
         String message,
+        @NotBlank
         String title,
         User author
 ) {
