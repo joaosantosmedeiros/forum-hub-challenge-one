9d9f4bd437fb26ca7d09d5587212344b4634e110
joaosantosmedeiros <jopesame@gmail.com>
2024-07-01 10:00:45 -0300

    feat: add topic repository

---
+++ src/main/java/com/jota/hub/challenge/domain/topic/TopicRepository.java
@@ -0,0 +1,6 @@
+package com.jota.hub.challenge.domain.topic;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TopicRepository extends JpaRepository<Topic, Long> {
+}
