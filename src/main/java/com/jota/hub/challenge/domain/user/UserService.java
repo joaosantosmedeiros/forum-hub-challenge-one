3a673072ef47323545a5b6d5e1981a300695df99
joaosantosmedeiros <jopesame@gmail.com>
2024-07-03 17:41:44 -0300

    feat: add user password encoding in user creation

--- src/main/java/com/jota/hub/challenge/domain/user/UserService.java
+++ src/main/java/com/jota/hub/challenge/domain/user/UserService.java
@@ -1,6 +1,8 @@
 package com.jota.hub.challenge.domain.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,12 +11,16 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public User create(User user) {
         var userExists = repository.findByEmail(user.getEmail());
         if(userExists.isPresent()){
             throw new IllegalArgumentException("Email already in use.");
         }
 
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return repository.save(user);
     }
 }
