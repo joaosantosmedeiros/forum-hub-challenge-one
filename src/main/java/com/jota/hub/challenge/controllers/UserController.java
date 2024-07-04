d3476b4293aa4990235613a99c4cb4f8a7b343fa
joaosantosmedeiros <jopesame@gmail.com>
2024-07-04 10:54:10 -0300

    feat: add user get endpoints

--- src/main/java/com/jota/hub/challenge/controllers/UserController.java
+++ src/main/java/com/jota/hub/challenge/controllers/UserController.java
@@ -6,12 +6,12 @@ import com.jota.hub.challenge.domain.user.UserService;
 import com.jota.hub.challenge.domain.user.dto.ReturnUserDTO;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -24,5 +24,16 @@ public class UserController {
     public ResponseEntity<ReturnUserDTO> create(@RequestBody @Valid CreateUserDTO dto) {
         User user = new User(null, dto.name(), dto.email(), dto.password(), true, null);
         return ResponseEntity.status(HttpStatus.CREATED).body(new ReturnUserDTO(userService.create(user)));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ReturnUserDTO>> list(@PageableDefault(sort = "name") Pageable pageable){
+        return ResponseEntity.ok(userService.list(pageable).map(ReturnUserDTO::new));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ReturnUserDTO> findById(@PathVariable(value = "id") Long id) {
+        var user = userService.findById(id);
+        return ResponseEntity.ok(new ReturnUserDTO(user));
     }
 }
--- src/main/java/com/jota/hub/challenge/domain/user/UserService.java
+++ src/main/java/com/jota/hub/challenge/domain/user/UserService.java
@@ -1,6 +1,8 @@
 package com.jota.hub.challenge.domain.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -26,6 +28,10 @@ public class UserService {
         return repository.save(user);
     }
 
+    public Page<User> list(Pageable pageable){
+        return repository.findAll(pageable);
+    }
+
     public User findByEmail(String email){
         var user = repository.findByEmail(email);
         if(user.isEmpty()){
@@ -33,5 +39,9 @@ public class UserService {
         }
 
         return user.get();
+    }
+
+    public User findById(Long id) {
+        return repository.findById(id).orElseThrow(() -> new NoSuchElementException("User does not exists."));
     }
 }
