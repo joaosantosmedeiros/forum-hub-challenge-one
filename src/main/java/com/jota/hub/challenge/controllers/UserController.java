39015d91b8ba23668c46c39e46ff7ca181fa1d87
joaosantosmedeiros <jopesame@gmail.com>
2024-07-04 19:01:38 -0300

    feat: add standard response in user controller

--- src/main/java/com/jota/hub/challenge/controllers/UserController.java
+++ src/main/java/com/jota/hub/challenge/controllers/UserController.java
@@ -1,13 +1,13 @@
 package com.jota.hub.challenge.controllers;
 
-import com.jota.hub.challenge.domain.user.dto.CreateUserDTO;
 import com.jota.hub.challenge.domain.user.User;
 import com.jota.hub.challenge.domain.user.UserService;
+import com.jota.hub.challenge.domain.user.dto.CreateUserDTO;
 import com.jota.hub.challenge.domain.user.dto.ReturnUserDTO;
 import com.jota.hub.challenge.domain.user.dto.UpdateUserDTO;
+import com.jota.hub.challenge.infra.dto.StandardResponseDTO;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -24,30 +24,47 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    public ResponseEntity<ReturnUserDTO> create(@RequestBody @Valid CreateUserDTO dto) {
+    public ResponseEntity<StandardResponseDTO<ReturnUserDTO>> create(@RequestBody @Valid CreateUserDTO dto) {
         User user = new User(null, dto.name(), dto.email(), dto.password(), true, null);
-        return ResponseEntity.status(HttpStatus.CREATED).body(new ReturnUserDTO(userService.create(user)));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(new StandardResponseDTO<>(
+                "User created successfully.",
+                true,
+                new ReturnUserDTO(userService.create(user))
+        ));
     }
 
     @GetMapping
-    public ResponseEntity<Page<ReturnUserDTO>> list(@PageableDefault(sort = "name") Pageable pageable){
-        return ResponseEntity.ok(userService.list(pageable).map(ReturnUserDTO::new));
+    public ResponseEntity<StandardResponseDTO<Page<ReturnUserDTO>>> list(@PageableDefault(sort = "name") Pageable pageable){
+        return ResponseEntity.ok(new StandardResponseDTO<>(
+                "Listing users.",
+                true,
+                userService.list(pageable).map(ReturnUserDTO::new)
+        ));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ReturnUserDTO> findById(@PathVariable(value = "id") Long id) {
+    public ResponseEntity<StandardResponseDTO<ReturnUserDTO>> findById(@PathVariable(value = "id") Long id) {
         var user = userService.findById(id);
-        return ResponseEntity.ok(new ReturnUserDTO(user));
+        return ResponseEntity.ok(new StandardResponseDTO<>(
+                "Showing found user.",
+                true,
+                new ReturnUserDTO(user)
+        ));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ReturnUserDTO> update(@PathVariable Long id, @RequestBody UpdateUserDTO dto) {
+    public ResponseEntity<StandardResponseDTO<ReturnUserDTO>> update(@PathVariable Long id, @RequestBody UpdateUserDTO dto) {
         var user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if(user.getId() != id){
             throw new SecurityException("User can only update itself.");
         }
         User updatedUser = userService.update(new User(id, dto.name(), dto.email(), dto.password(), true, null));
-        return ResponseEntity.ok(new ReturnUserDTO(updatedUser));
+        return ResponseEntity.ok(new StandardResponseDTO<>(
+                "User updated successfully",
+                true,
+                new ReturnUserDTO(updatedUser)
+        ));
     }
 
     @DeleteMapping("/{id}")
