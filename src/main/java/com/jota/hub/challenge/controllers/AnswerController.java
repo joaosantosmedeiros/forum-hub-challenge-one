64891f3e41ea0d5ee3d7703268635af1477449b0
joaosantosmedeiros <jopesame@gmail.com>
2024-07-04 18:36:17 -0300

    feat: add standard response in answer controller

--- src/main/java/com/jota/hub/challenge/controllers/AnswerController.java
+++ src/main/java/com/jota/hub/challenge/controllers/AnswerController.java
@@ -7,6 +7,7 @@ import com.jota.hub.challenge.domain.answer.dto.UpdateAnswerDTO;
 import com.jota.hub.challenge.domain.topic.TopicService;
 import com.jota.hub.challenge.domain.user.User;
 import com.jota.hub.challenge.domain.user.UserService;
+import com.jota.hub.challenge.infra.dto.StandardResponseDTO;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -27,33 +28,49 @@ public class AnswerController {
     private UserService userService;
 
     @PostMapping
-    public ResponseEntity<AnswerDTO> create(@RequestBody @Valid AnswerDTO dto) {
+    public ResponseEntity<StandardResponseDTO<AnswerDTO>> create(@RequestBody @Valid AnswerDTO dto) {
         var topic = topicService.findById(dto.topicId());
         var user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Answer answer = answerService.create(new Answer(null, dto.message(), topic, LocalDateTime.now(), user));
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(new AnswerDTO(answer));
+        return ResponseEntity.status(HttpStatus.CREATED).body(new StandardResponseDTO<>(
+                "Answer created successfully.",
+                true,
+                new AnswerDTO(answer)
+        ));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<AnswerDTO> findById(@PathVariable Long id) {
+    public ResponseEntity<StandardResponseDTO<AnswerDTO>> findById(@PathVariable Long id) {
         Answer answer = answerService.findById(id);
-        return ResponseEntity.ok(new AnswerDTO(answer));
+        return ResponseEntity.ok(new StandardResponseDTO<>(
+                "Showing found answer.",
+                true,
+                new AnswerDTO(answer)
+        ));
     }
 
     @GetMapping("/autor/{id}")
-    public ResponseEntity<List<AnswerDTO>> findByAuthor(@PathVariable Long id){
+    public ResponseEntity<StandardResponseDTO<List<AnswerDTO>>> findByAuthor(@PathVariable Long id){
         User author = userService.findById(id);
         List<Answer> answers = answerService.findByAuthor(author);
 
-        return ResponseEntity.ok(answers.stream().map(AnswerDTO::new).toList());
+        return ResponseEntity.ok(new StandardResponseDTO<>(
+                "Showing responses by author.",
+                true,
+                answers.stream().map(AnswerDTO::new).toList()
+        ));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<AnswerDTO> update(@PathVariable Long id, @RequestBody @Valid UpdateAnswerDTO dto){
+    public ResponseEntity<StandardResponseDTO<AnswerDTO>> update(@PathVariable Long id, @RequestBody @Valid UpdateAnswerDTO dto){
         User author = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Answer answer = answerService.update(new Answer(id, dto.message(), null, null, author));
-        return ResponseEntity.ok(new AnswerDTO(answer));
+        return ResponseEntity.ok(new StandardResponseDTO<>(
+                "Answer updated successfully",
+                true,
+                new AnswerDTO(answer))
+        );
     }
 
     @DeleteMapping("/{id}")
