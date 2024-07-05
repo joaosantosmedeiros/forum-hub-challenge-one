520b30c070289ddd1f618585df8352c2d7b1c303
joaosantosmedeiros <jopesame@gmail.com>
2024-07-05 10:16:35 -0300

    feat: add course controller spring doc definitions

--- src/main/java/com/jota/hub/challenge/controllers/CourseController.java
+++ src/main/java/com/jota/hub/challenge/controllers/CourseController.java
@@ -5,6 +5,14 @@ import com.jota.hub.challenge.domain.course.CourseService;
 import com.jota.hub.challenge.domain.course.dtos.CourseDTO;
 import com.jota.hub.challenge.domain.course.dtos.UpdateCourseDTO;
 import com.jota.hub.challenge.infra.dto.StandardResponseDTO;
+import com.jota.hub.challenge.infra.springdoc.schemas.DefaultErrorResponseSchema;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -14,6 +22,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Courses", description = "Operations related to courses.")
+@SecurityRequirement(name = "bearer-key")
 @RestController
 @RequestMapping("/cursos")
 @AllArgsConstructor
@@ -22,6 +32,21 @@ public class CourseController {
     private CourseService courseService;
 
     @PostMapping
+    @Operation(description = "Course creation.", summary = "Create a new course.")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Course created"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid data provided.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = DefaultErrorResponseSchema.class)
+                    )
+            )
+    })
     public ResponseEntity<StandardResponseDTO<CourseDTO>> create(@RequestBody @Valid CourseDTO courseDTO) {
 
         var course = courseService.create(new Course(
@@ -40,6 +65,13 @@ public class CourseController {
     }
 
     @GetMapping
+    @Operation(description = "List courses.", summary = "Courses listing.")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Success."
+            ),
+    })
     public ResponseEntity<StandardResponseDTO<Page<CourseDTO>>> list(@PageableDefault(sort = "id") Pageable pageable){
         return ResponseEntity.ok(new StandardResponseDTO<>(
                 "Listing courses.",
@@ -49,6 +81,21 @@ public class CourseController {
     }
 
     @GetMapping("/{id}")
+    @Operation(description = "Course search.", summary = "Search for a course by its id.")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Success."
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Course not found.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = DefaultErrorResponseSchema.class)
+                    )
+            )
+    })
     public ResponseEntity<StandardResponseDTO<CourseDTO>> findById(@PathVariable(value = "id") Long id){
         var course = courseService.findById(id);
         return ResponseEntity.ok(new StandardResponseDTO<>(
@@ -59,6 +106,29 @@ public class CourseController {
     }
 
     @PutMapping("/{id}")
+    @Operation(description = "Course update.", summary = "Update a course.")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Success."
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid data provided",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = DefaultErrorResponseSchema.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Course not found.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = DefaultErrorResponseSchema.class)
+                    )
+            )
+    })
     public ResponseEntity<StandardResponseDTO<CourseDTO>> update(@PathVariable Long id, @RequestBody UpdateCourseDTO dto) {
 
         Course course = courseService.update(new Course(id, dto.name(), dto.category(), true, null));
@@ -71,6 +141,29 @@ public class CourseController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(description = "Course delete.", summary = "Delete a course.")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Success."
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "User tries to delete an inactive course.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = DefaultErrorResponseSchema.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Course not found.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = DefaultErrorResponseSchema.class)
+                    )
+            )
+    })
     public void delete(@PathVariable(value = "id") Long id){
         courseService.delete(id);
     }
